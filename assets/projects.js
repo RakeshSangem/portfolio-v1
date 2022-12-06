d7d2842da9046f3d55e825218bfe41ec6d1f5f94
Rakesh Sangem <107752425+RakeshSangem@users.noreply.github.com>
2022-12-06 18:44:13 +0530

    add links to images

--- assets/projects.js
+++ assets/projects.js
@@ -13,7 +13,7 @@ export default [
     description:
       "Bankist is an online platform where user can access their bank account details, transfer money and much more.",
     image:
-      "https://user-images.githubusercontent.com/54346106/193443537-deab1b57-450d-4a1b-885e-0bb417b09882.png",
+      "https://user-images.githubusercontent.com/107752425/205921327-16fa9da1-d602-4b18-8cce-6f80c4585fd2.png",
     github: "https://github.com/rakeshsangem/bankist-application",
     live: "https://bankist-application.vercel.app",
   },
@@ -22,7 +22,7 @@ export default [
     description:
       "A portflio website of me buit using HTML, CSS, and JavaScript. I made to opnesource during hacktoberfest",
     image:
-      "https://user-images.githubusercontent.com/54346106/193443537-deab1b57-450d-4a1b-885e-0bb417b09882.png",
+      "https://user-images.githubusercontent.com/107752425/192083103-e773c142-6ed0-4223-8fca-62525c2f908a.png",
     github: "https://github.com/rakeshsangem/portflio",
     live: "https://www.rakeshsangem.me",
   },
