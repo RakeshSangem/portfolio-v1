bd1e12fc81f6abe421f321348e55d87352082c7d
Rakesh Sangem <107752425+RakeshSangem@users.noreply.github.com>
2022-12-06 17:34:29 +0530

    fix: border radius

--- assets/projects.js
+++ assets/projects.js
@@ -1,7 +1,8 @@
 export default [
   {
     title: "Sharkfolio",
-    description: "dskjfhlkds sdfhlkdsf",
+    description:
+      "A portfolio website of sharks. During the MLH hackathon, it was built with React, Tailwind CSS, and GSAP.",
     image:
       "https://user-images.githubusercontent.com/54346106/193443537-deab1b57-450d-4a1b-885e-0bb417b09882.png",
     github: "https://github.com/rakeshsangem/harkfolio",
@@ -10,7 +11,7 @@ export default [
   {
     title: "Bankist",
     description:
-      "Bankist is an online platform where user can interact and access their bank account details and much more",
+      "Bankist is an online platform where user can access their bank account details, transfer money and much more.",
     image:
       "https://user-images.githubusercontent.com/54346106/193443537-deab1b57-450d-4a1b-885e-0bb417b09882.png",
     github: "https://github.com/rakeshsangem/bankist-application",
