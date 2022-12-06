08920c4c739ba6f2943a3ce1a15db249c23aa293
Rakesh Sangem <107752425+RakeshSangem@users.noreply.github.com>
2022-12-06 17:07:52 +0530

    fix:project card

--- app.js
+++ app.js
@@ -1,53 +1,95 @@
+import projects from "./assets/projects.js";
+
+const projectsContainer = document.querySelector(".project-cards");
+
 const tl = gsap.timeline();
 
-tl.from('.name', {
-    x: -100,
-    opacity: 0,
-    duration: .8
+tl.from(".name", {
+  x: -100,
+  opacity: 0,
+  duration: 0.8,
 })
-.from('.job', {
+  .from(
+    ".job",
+    {
+      x: -100,
+      opacity: 0,
+      duration: 1,
+    },
+    "-=.3"
+  )
+  .from(".job-2", {
     x: -100,
     opacity: 0,
-    duration: 1
-}, "-=.3")
-.from('.job-2', {
-    x: -100,
-    opacity: 0,
-    duration: .2
-})
-.from('.job-1', {
-    x: -100,
-    opacity: 0,
-    duration: .2
-}, '-=.2')
-.from('.btn-1', {
+    duration: 0.2,
+  })
+  .from(
+    ".job-1",
+    {
+      x: -100,
+      opacity: 0,
+      duration: 0.2,
+    },
+    "-=.2"
+  )
+  .from(".btn-1", {
     y: 100,
     opacity: 0,
-    duration: .3
-})
-.from('.btn-2', {
-    y: 100,
-    opacity: 0,
-    duration: .3
-}, '-=.2')
+    duration: 0.3,
+  })
+  .from(
+    ".btn-2",
+    {
+      y: 100,
+      opacity: 0,
+      duration: 0.3,
+    },
+    "-=.2"
+  );
 
-gsap.from('.project-card', {
-    scrollTrigger: {
-        trigger: ".projects",
-        start: "top center"
-    },
-    y: 150,
-    opacity: 0,
-    stagger: .2,
-    duration: 1
-})
-gsap.from('.contact-container', {
-    scrollTrigger: {
-        trigger: ".contact-page",
-        start: "top center"
-    },
-    y: 150,
-    opacity: 0,
-    stagger: .2,
-    duration: 1.3
-})
+gsap.from(".project-card", {
+  scrollTrigger: {
+    trigger: ".projects",
+    start: "top center",
+  },
+  y: 150,
+  opacity: 0,
+  stagger: 0.2,
+  duration: 1,
+});
+gsap.from(".contact-container", {
+  scrollTrigger: {
+    trigger: ".contact-page",
+    start: "top center",
+  },
+  y: 150,
+  opacity: 0,
+  stagger: 0.2,
+  duration: 1.3,
+});
+
+projects.map((item) => {
+  console.log(item);
+});
+
+const printProjects = () => {
+  projects.map((item) => {
+    projectsContainer.innerHTML += `
+        <article class="project-card">
+            <div class="imageContainer">
+              <img src="${item.image}" alt="" />
+            </div>
+            <div class="project-details">
+              <h2>${item.title}</h2>
+              <p>${item.description}</p>
+            </div>
+            <div class="project-links">
+              <a href="${item.github}">Github</a>
+              <a href="${item.live}">Live site</a>
+            </div>
+        </article>
+    
+    `;
+  });
+};
+printProjects();
---
+++ assets/projects.js
@@ -0,0 +1,28 @@
+export default [
+  {
+    title: "Sharkfolio",
+    description: "dskjfhlkds sdfhlkdsf",
+    image:
+      "https://user-images.githubusercontent.com/54346106/193443537-deab1b57-450d-4a1b-885e-0bb417b09882.png",
+    github: "https://github.com/rakeshsangem/harkfolio",
+    live: "https://www.sharkfolio.vercel.app",
+  },
+  {
+    title: "Bankist",
+    description:
+      "Bankist is an online platform where user can interact and access their bank account details and much more",
+    image:
+      "https://user-images.githubusercontent.com/54346106/193443537-deab1b57-450d-4a1b-885e-0bb417b09882.png",
+    github: "https://github.com/rakeshsangem/bankist-application",
+    live: "https://www.bankist-application.vercel.app",
+  },
+  {
+    title: "Portfolio",
+    description:
+      "A portflio website of me buit using HTML, CSS, and JavaScript. I made to opnesource during hacktoberfest",
+    image:
+      "https://user-images.githubusercontent.com/54346106/193443537-deab1b57-450d-4a1b-885e-0bb417b09882.png",
+    github: "https://github.com/rakeshsangem/portflio",
+    live: "https://www.rakeshsangem.me",
+  },
+];
